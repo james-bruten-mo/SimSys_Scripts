f54fb95b1cd0ca53b83d7d78d978a383210f9b8d
James Bruten <james.bruten@metoffice.gov.uk>
2025-08-27 10:23:06 +0100

    add new file

---
+++ github_nightly_testing/github_nightly_testing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Launch a nightly test suite of the github test suite migration branches.
+Merge the branch into the latest copy of trunk - send a message if conflicts occur
+Temporary - edit the location of the clone
+Launch a simple rose-stem suite
+Apps,
+UM,
+Jules
+"""
